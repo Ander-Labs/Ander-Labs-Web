dadd6420091b41565b9c6e5d42c74a1f3f7dd0d9
andersonruizr2 <andersonruizr2@gmail.com>
2024-06-29 17:06:13 +0000

    update file

---
+++ .idx/dev.nix
@@ -0,0 +1,74 @@
+# To learn more about how to use Nix to configure your environment
+# see: https://developers.google.com/idx/guides/customize-idx-env
+{ pkgs, ... }: {
+  # Which nixpkgs channel to use.
+  channel = "stable-23.11"; # or "unstable"
+
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    # pkgs.go
+    # pkgs.python311
+    # pkgs.python311Packages.pip
+    # pkgs.nodejs_20
+    # pkgs.nodePackages.nodemon
+  ];
+
+  # Sets environment variables in the workspace
+  env = {};
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [
+      # "vscodevim.vim"
+      "aaron-bond.better-comments"
+      "astro-build.astro-vscode"
+      "bradlc.vscode-tailwindcss"
+      "dbaeumer.vscode-eslint"
+      "esbenp.prettier-vscode"
+      "formulahendry.auto-close-tag"
+      "formulahendry.auto-complete-tag"
+      "formulahendry.auto-rename-tag"
+      "formulahendry.code-runner"
+      "mhutchie.git-graph"
+      "ms-vscode.js-debug"
+      "ms-vscode.vscode-typescript-next"
+      "oderwat.indent-rainbow"
+      "PKief.material-icon-theme"
+      "shardulm94.trailing-spaces"
+      "teabyii.ayu"
+      "usernamehw.errorlens"
+      "yzhang.markdown-all-in-one"
+      "chamboug.js-auto-backticks"
+    ];
+
+    # Enable previews
+    previews = {
+      enable = true;
+      previews = {
+        # web = {
+        #   # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
+        #   # and show it in IDX's web preview panel
+        #   command = ["npm" "run" "dev"];
+        #   manager = "web";
+        #   env = {
+        #     # Environment variables to set for your server
+        #     PORT = "$PORT";
+        #   };
+        # };
+      };
+    };
+
+    # Workspace lifecycle hooks
+    workspace = {
+      # Runs when a workspace is first created
+      onCreate = {
+        # Example: install JS dependencies from NPM
+        # npm-install = "npm install";
+      };
+      # Runs when the workspace is (re)started
+      onStart = {
+        # Example: start a background task to watch and re-build backend code
+        # watch-backend = "npm run watch-backend";
+      };
+    };
+  };
+}
